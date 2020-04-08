9017e1b3822cbceb6e6aa62aa025706f59427cfa
TenType <55125103+TenType@users.noreply.github.com>
2020-04-07 19:38:58 -0700

    Update script.js

--- notepad/script.js
+++ notepad/script.js
@@ -1,2 +1,3 @@
 document.getElementById('textarea').value = localStorage.getItem('content');
+document.getElementById('textarea').addEventListener("keyup", 
 localStorage.setItem('content', document.getElementById('textarea').value));
